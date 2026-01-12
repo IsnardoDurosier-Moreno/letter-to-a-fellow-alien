45326d7886d92a32ace0fa7ee3c6aee51ad2a3b8
IsnardoDurosier-Moreno <izzydurosier@gmail.com>
2026-01-12 13:50:59 -0500

    Initial alien letter site

---
+++ script.js
@@ -0,0 +1,29 @@
+function goToLetter() {
+  window.location.href = "letter.html";
+  }
+  
+function goToPrompt() {
+  window.location.href = "prompt.html";
+  }
+  
+function goToPrompt2() {
+  window.location.href = "prompt2.html";
+  }
+function goToPrompt3() {
+  window.location.href = "prompt3.html";
+  }
+function goToPrompt4() {
+  window.location.href = "prompt4.html";
+  }
+function goToPrompt5() {
+  window.location.href = "prompt5.html";
+  }
+function goToPrompt6() {
+  window.location.href = "prompt6.html";
+  }
+function goToPrompt7(){
+  window.location.href = "prompt7.html";
+  }
+function goToPrompt8(){
+  window.location.href = "prompt8.html";
+  }
